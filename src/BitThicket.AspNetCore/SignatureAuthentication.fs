5f217b2a557ed03fb2a19777081839ebf0557e02
Ben Collins <aggieben@gmail.com>
2019-12-11 19:46:07 -0600

    add refereence to caching package; simplify signatureauthenticationoptions

--- src/BitThicket.AspNetCore/SignatureAuthentication.fs
+++ src/BitThicket.AspNetCore/SignatureAuthentication.fs
@@ -9,12 +9,15 @@ open System.Text.RegularExpressions
 open System.Threading.Tasks
 open Microsoft.AspNetCore.Authentication
 open Microsoft.AspNetCore.Http
+open Microsoft.Extensions.Caching.Distributed
 open Microsoft.Extensions.Logging
+
 open Microsoft.Net.Http.Headers
 open FSharp.Control.Tasks.V2
 open FSharp.Data.UnitSystems.SI.UnitSymbols
 
 open Garage.FSharp.Result
+open Garage.FSharp.TaskResult
 
 type SignatureAlgorithm =
     | HmacSha256
@@ -24,16 +27,18 @@ type SignatureAlgorithm =
             | "hmac-sha256" -> Some HmacSha256 
             | _ -> None
 
-type IClientSecretProvider<'TClientKey> =
-    abstract GetClientSecretAsync : 'TClientKey -> Task<byte[] option>
+type IClientSecretProvider =
+    abstract GetClientSecretAsync : string -> Task<byte[] option>
 
-type SignatureAuthenticationOptions<'TClientKey>
-    (secretProvider:IClientSecretProvider<'TClientKey>, ?realm:string, ?algorithms:SignatureAlgorithm[], ?maxSkew:int<s>) =
+type SignatureAuthenticationOptions
+    (secretProvider:IClientSecretProvider, ?realm:string, ?algorithms:SignatureAlgorithm[], ?maxSkew:int<s>) =
     inherit AuthenticationSchemeOptions()
     member val Realm = defaultArg realm String.Empty with get,set
     member val SupportedAlgorithms = defaultArg algorithms [| HmacSha256 |] with get,set
     member val MaxClockSkew = defaultArg maxSkew 600<s> with get,set
     member __.ClientSecretProvider = secretProvider
+
+    new() = SignatureAuthenticationOptions(null)
 
 type UnvalidatedSignatureEnvelopeParsingError =
     | MissingHeaderValue
@@ -160,7 +165,7 @@ module SignatureHelpers =
                   expires = None
                   headers = None }} |> Ok
 
-    let private validateAlgorithm<'tkey> (options:SignatureAuthenticationOptions<'tkey>) (state:SignatureEnvelopeValidationState) =
+    let private validateAlgorithm (options:SignatureAuthenticationOptions) (state:SignatureEnvelopeValidationState) =
         match state.unvalidatedEnvelope.algorithm with
         | None -> Ok state
         | Some algorithmName -> 
@@ -171,7 +176,7 @@ module SignatureHelpers =
                 then Ok {state with validatedEnvelope = { state.validatedEnvelope with algorithm = Some algo }}
                 else InvalidAlgorithm |> Error
 
-    let private validateCreated<'tkey> (options:SignatureAuthenticationOptions<'tkey>) (state:SignatureEnvelopeValidationState) =
+    let private validateCreated (options:SignatureAuthenticationOptions) (state:SignatureEnvelopeValidationState) =
         // §2.1.4: must be unix timestamp.  future timestamp must fail.  second precision.
         match state.unvalidatedEnvelope.created with
         | None -> Ok state
@@ -192,7 +197,7 @@ module SignatureHelpers =
                 with
                 | e -> InvalidCreatedTimestamp e.Message |> Error
 
-    let private validateExpires<'tkey> (options:SignatureAuthenticationOptions<'tkey>) (state:SignatureEnvelopeValidationState) =
+    let private validateExpires (options:SignatureAuthenticationOptions) (state:SignatureEnvelopeValidationState) =
         // §2.1.5: must be unix integer, subsecond allowed. past timestamp fails
         match state.unvalidatedEnvelope.expires with
         | None -> Ok state
@@ -212,7 +217,7 @@ module SignatureHelpers =
                 with
                 | e -> InvalidExpiresTimestamp e.Message |> Error
 
-    let private validateHeaders<'tkey> (options:SignatureAuthenticationOptions<'tkey>) (state:SignatureEnvelopeValidationState) =
+    let private validateHeaders (options:SignatureAuthenticationOptions) (state:SignatureEnvelopeValidationState) =
         // §2.1.6: if not specified, then default is "(created)".  empty is different that non-specified
         match state.unvalidatedEnvelope.headers with
         | None -> Ok state
@@ -226,7 +231,7 @@ module SignatureHelpers =
                                       |> Option.ofObj } } 
                  |> Ok
 
-    let validateSignatureEnvelope<'tkey> (options:SignatureAuthenticationOptions<'tkey>) (usigenv:UnvalidatedSignatureEnvelope) = 
+    let validateSignatureEnvelope (options:SignatureAuthenticationOptions) (usigenv:UnvalidatedSignatureEnvelope) = 
         validateRequiredParams usigenv
         >>*= validateAlgorithm options
         >>*= validateCreated options
@@ -234,140 +239,138 @@ module SignatureHelpers =
         >>*= validateHeaders options
         |> Result.map (fun state -> state.validatedEnvelope)
 
-    // let private ensureClientSecretAsync (secretProvider:IClientSecretProvider) state = task {
-    //     let envelope = state.envelope.Value
-    //     match! repository.GetClientSecret(envelope.keyId) with
-    //     | None -> return InvalidClient |> Error
-    //     | Some secret ->
-    //         let key = Encoding.UTF8.GetBytes(secret)
-    //         return { state with clientSecret = Some key } |> Ok
-    // }
+    let private ensureClientSecretAsync (secretProvider:IClientSecretProvider) state = task {
+        let envelope = state.envelope.Value
+        match! secretProvider.GetClientSecretAsync(envelope.keyId) with
+        | None -> return InvalidClient |> Error
+        | Some secret ->
+            return { state with clientSecret = Some secret } |> Ok
+    }
 
-//     let resolveRequestTarget (req:HttpRequest) =
-//         req.Path.Add(req.QueryString)
-//         |> sprintf "%s %s" (req.Method.ToLowerInvariant())
+    let resolveRequestTarget (req:HttpRequest) =
+        req.Path.Add(req.QueryString)
+        |> sprintf "%s %s" (req.Method.ToLowerInvariant())
 
-    
-//     let resolveHeaderValue (name:string) (headers:IHeaderDictionary) =
-//         match headers.Keys |> Seq.tryFind (fun k -> k.ToLowerInvariant() = name) with
-//         | None -> None
-//         | Some key -> 
-//             headers.[key]
-//             |> Seq.map (fun s -> s.Trim())
-//             |> (fun strings -> String.Join(", ", strings).Trim())
-//             |> Some
+    let resolveHeaderValue (name:string) (headers:IHeaderDictionary) =
+        match headers.Keys |> Seq.tryFind (fun k -> k.ToLowerInvariant() = name) with
+        | None -> None
+        | Some key -> 
+            headers.[key]
+            |> Seq.map (fun s -> s.Trim())
+            |> (fun strings -> String.Join(", ", strings).Trim())
+            |> Some
 
-//     let private resolveSignatureDataField (field:string) (validationState:SignatureValidationState) (stringState:string) =
-//         try
-//             match field with
-//             | "(request-target)" -> 
-//                 stringState 
-//                 + (resolveRequestTarget validationState.request.Value) 
-//                 + " "
-//                 |> Ok
-//             | "(created)" ->
-//                 // I'm intentionally ignoring §2.3.2 here because it's 
-//                 // (a) unclear and (b) of dubious benefit, afaict
-//                 stringState
-//                 + validationState.envelope.Value.created.Value.ToUnixTimeSeconds().ToString()
-//                 + " "
-//                 |> Ok
-//                 // Ditto, I'm ignoring §2.3.3 for the same reasons
-//             | "(expires)" ->
-//                 stringState
-//                 + validationState.envelope.Value.expires.Value.ToUnixTimeSeconds().ToString()
-//                 + " "
-//                 |> Ok
-//             | field -> 
-//                 // §2.3.4: concatenate "name: value"
-//                 let req = validationState.request.Value
-//                 match resolveHeaderValue field req.Headers with
-//                 | None -> sprintf "header not found for field: %s" field |> Error
-//                 | Some value -> 
-//                     stringState 
-//                     + field + ": " + value 
-//                     + " " 
-//                     |> Ok
-//         with
-//         | e -> sprintf "invalid signature field: %s" e.Message |> Error
+    let private resolveSignatureDataField (field:string) (validationState:SignatureValidationState) (stringState:string) =
+        try
+            match field with
+            | "(request-target)" -> 
+                stringState 
+                + (resolveRequestTarget validationState.request.Value) 
+                + " "
+                |> Ok
+            | "(created)" ->
+                // I'm intentionally ignoring §2.3.2 here because it's 
+                // (a) unclear and (b) of dubious benefit, afaict
+                stringState
+                + validationState.envelope.Value.created.Value.ToString()
+                + " "
+                |> Ok
+                // Ditto, I'm ignoring §2.3.3 for the same reasons
+            | "(expires)" ->
+                stringState
+                + validationState.envelope.Value.expires.Value.ToString()
+                + " "
+                |> Ok
+            | field -> 
+                // §2.3.4: concatenate "name: value"
+                let req = validationState.request.Value
+                match resolveHeaderValue field req.Headers with
+                | None -> sprintf "header not found for field: %s" field |> Error
+                | Some value -> 
+                    stringState 
+                    + field + ": " + value 
+                    + " " 
+                    |> Ok
+        with
+        | e -> sprintf "invalid signature field: %s" e.Message |> Error
 
-//     let private constructSignatureData (validationState:SignatureValidationState) =
-//         Option.defaultValue [| "(created)" |] validationState.envelope.Value.headers
-//         |> Array.fold
-//             (fun state field ->
-//                 Result.bindOk (resolveSignatureDataField field validationState) state)
-//             (Ok String.Empty)
-//         |> Result.map (fun ss -> ss.Trim())
-//         |> Result.map Encoding.UTF8.GetBytes
+    let private constructSignatureData (validationState:SignatureValidationState) =
+        Option.defaultValue [| "(created)" |] validationState.envelope.Value.headers
+        |> Array.fold
+            (fun state field ->
+                bindOk (resolveSignatureDataField field validationState) state)
+            (Ok String.Empty)
+        |> Result.map (fun ss -> ss.Trim())
+        |> Result.map Encoding.UTF8.GetBytes
 
-//     let private computeCheckSignature state = 
-//         match constructSignatureData state with
-//         | Error msg -> InvalidSignatureString msg |> Error
-//         | Ok sigdata -> 
-//             try
-//                 use hmac = new HMACSHA256(state.clientSecret.Value)
-//                 { state with
-//                     checkSignature = hmac.ComputeHash(sigdata) |> Some }
-//                 |> Ok
-//             with
-//             | e -> HashError e.Message |> Error
+    let private computeCheckSignature state = 
+        match constructSignatureData state with
+        | Error msg -> InvalidSignatureString msg |> Error
+        | Ok sigdata -> 
+            try
+                use hmac = new HMACSHA256(state.clientSecret.Value)
+                { state with
+                    checkSignature = hmac.ComputeHash(sigdata) |> Some }
+                |> Ok
+            with
+            | e -> HashError e.Message |> Error
 
-//     let private compareSignatures state =
-//         let sigSpan = ReadOnlySpan(state.envelope.Value.signature)
-//         let checkSigSpan = ReadOnlySpan(state.checkSignature.Value)
-//         match checkSigSpan.SequenceCompareTo(sigSpan) with
-//         | 0 -> Ok state
-//         | _ -> Error InvalidSignature
+    let private compareSignatures state =
+        let sigSpan = ReadOnlySpan(state.envelope.Value.signature)
+        let checkSigSpan = ReadOnlySpan(state.checkSignature.Value)
+        match checkSigSpan.SequenceCompareTo(sigSpan) with
+        | 0 -> Ok state
+        | _ -> Error InvalidSignature
 
-//     let validateSignature (repository:IRepository) (options:SignatureAuthenticationOptions) (req:HttpRequest) (sigenv:SignatureEnvelope) =
-//         { SignatureValidationState.Default 
-//             with 
-//                 request = Some req
-//                 envelope = Some sigenv }
-//         |> ensureClientSecretAsync repository
-//         <*-> computeCheckSignature
-//         <*-> compareSignatures
-//         <*>  (fun state -> state.envelope.Value.keyId) // drop state
+    let validateSignature (secretProvider:IClientSecretProvider) (options:SignatureAuthenticationOptions) (req:HttpRequest) (sigenv:SignatureEnvelope) =
+        { SignatureValidationState.Default 
+            with 
+                request = Some req
+                envelope = Some sigenv }
+        |> ensureClientSecretAsync secretProvider
+        <*-> computeCheckSignature
+        <*-> compareSignatures
+        <*>  (fun state -> state.envelope.Value.keyId) // drop state
 
 // // https://datatracker.ietf.org/doc/draft-cavage-http-signatures/?include_text=1
 
-// type SignatureAuthenticationHandler(options, loggerFactory, encoder, clock, cache:IDistributedCache, repository:IRepository) = 
-//     inherit AuthenticationHandler<SignatureAuthenticationOptions>(options, loggerFactory, encoder, clock)
-//         override this.HandleAuthenticateAsync() = 
-//             // this is necessary because the compiler machinery will place the expressions in the computational
-//             // expression below into a lambda, which takes them _out_ of the context of the class where the this
-//             // binding is accessible.  This binding is then captured as part of the closure for the CE
-//             let request = this.Request
-//             let logger = loggerFactory.CreateLogger<SignatureAuthenticationHandler>()
-//             let currentOptions = options.CurrentValue
-//             task {
-//                 match SignatureHelpers.getUnvalidatedSignatureEnvelope request with
-//                 | Error e -> 
-//                     logger.LogError("Error getting signature envelope: {0}", sprintf "%A" e)
-//                     return AuthenticateResult.NoResult()
-//                 | Ok unvalidatedEnvelope -> 
-//                     match SignatureHelpers.validateSignatureEnvelope currentOptions unvalidatedEnvelope with
-//                     | Error e ->
-//                         logger.LogError("Error validating signature: {0}", sprintf "%A" e)
-//                         return AuthenticateResult.NoResult()
-//                     | Ok envelope ->
-//                         let! validationResult = 
-//                             SignatureHelpers.validateSignature repository currentOptions request envelope
+type SignatureAuthenticationHandler(options, loggerFactory, encoder, clock, cache:IDistributedCache) = 
+    inherit AuthenticationHandler<SignatureAuthenticationOptions>(options, loggerFactory, encoder, clock)
+        override this.HandleAuthenticateAsync() = 
+            // this is necessary because the compiler machinery will place the expressions in the computational
+            // expression below into a lambda, which takes them _out_ of the context of the class where the this
+            // binding is accessible.  This binding is then captured as part of the closure for the CE
+            // let request = this.Request
+            // let logger = loggerFactory.CreateLogger<SignatureAuthenticationHandler>()
+            // let currentOptions = options.CurrentValue
+            // task {
+            //     match SignatureHelpers.getUnvalidatedSignatureEnvelope request with
+            //     | Error e -> 
+            //         logger.LogError("Error getting signature envelope: {0}", sprintf "%A" e)
+            //         return AuthenticateResult.NoResult()
+            //     | Ok unvalidatedEnvelope -> 
+            //         match SignatureHelpers.validateSignatureEnvelope currentOptions unvalidatedEnvelope with
+            //         | Error e ->
+            //             logger.LogError("Error validating signature: {0}", sprintf "%A" e)
+            //             return AuthenticateResult.NoResult()
+            //         | Ok envelope ->
+            //             let! validationResult = 
+            //                 SignatureHelpers.validateSignature repository currentOptions request envelope
 //                         match validationResult with
 //                         | Error err -> return AuthenticateResult.Fail (sprintf "%A" err)
 //                         | Ok clientId ->
 //                             let! principal = this.GetClaimsPrincipalForClient(clientId)
 //                             let ticket = AuthenticationTicket(principal, this.Scheme.Name)
 //                             return AuthenticateResult.Success(ticket)
-//             }
+            // }
 
-//     member this.GetClaimsPrincipalForClient(clientId:string) =
-//         let scheme = this.Scheme
-//         task {
-//             return 
-//                 ClaimsPrincipal(ClaimsIdentity([|
-//                     Claim(ClaimTypes.Name, clientId)
-//                 |], scheme.Name)) }
+    member this.GetClaimsPrincipalForClient(clientId:string) =
+        let scheme = this.Scheme
+        task {
+            return 
+                ClaimsPrincipal(ClaimsIdentity([|
+                    Claim(ClaimTypes.Name, clientId)
+                |], scheme.Name)) }
 
 
 
