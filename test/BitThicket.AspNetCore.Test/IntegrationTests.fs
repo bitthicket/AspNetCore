f002cd5e5f8f175a1291c94c3b1b6fb1f8478deb
Ben Collins <aggieben@gmail.com>
2019-12-16 12:52:06 -0600

    [wip] trying to get integration test working

--- test/BitThicket.AspNetCore.Test/IntegrationTests.fs
+++ test/BitThicket.AspNetCore.Test/IntegrationTests.fs
@@ -2,9 +2,11 @@ module IntegrationTests
 
 open System
 open System.Net
+open System.Threading.Tasks
 open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Hosting
 open Microsoft.AspNetCore.Hosting.Server
+open Microsoft.AspNetCore.Http
 open Microsoft.AspNetCore.TestHost
 open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.DependencyInjection
@@ -20,20 +22,30 @@ open BitThicket.AspNetCore.Authentication
 [<Fact>]
 [<Trait("Category", "Integration")>]
 let ``signature authenticated request to root``() = task {
+    let clientSecret = Guid.NewGuid().ToByteArray()
+
     let builder = 
         WebHostBuilder()
             .ConfigureServices(
                 fun services -> 
+                    services.AddDistributedMemoryCache() |> ignore
                     services.AddAuthentication("Signature")
                         .AddScheme<SignatureAuthenticationOptions, SignatureAuthenticationHandler>("Signature", 
                             fun (opts:SignatureAuthenticationOptions) -> 
                                 opts.Realm <- "Test"
-                                opts.ClientSecretProvider <- 
+                                // opts.ClientSecretProvider <- { new IClientSecretProvider with
+                                //                                 member __.GetClientSecretAsync(_) = 
+                                //                                     Some clientSecret
+                                //                                     |> Task.FromResult }
+                                ())
                     |> ignore)
                         
             .Configure(
-                fun app -> 
-                    app |> ignore)
+                fun app ->
+                    // app.UseAuthentication() |> ignore
+                    app.Run(fun context -> context.Response.WriteAsync("Hello World")) |> ignore
+                    app.UseAuthentication()
+                    |> ignore)
 
     use server = new TestServer(builder)
 
