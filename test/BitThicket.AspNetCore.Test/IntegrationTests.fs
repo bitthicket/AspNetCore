77bb8bb3a55326e48ac5206d079fd03372b12ff4
Ben Collins <aggieben@gmail.com>
2019-12-17 17:11:55 -0600

    rework integration test to properly log, as well as account for authentication requirement.

--- test/BitThicket.AspNetCore.Test/IntegrationTests.fs
+++ test/BitThicket.AspNetCore.Test/IntegrationTests.fs
@@ -10,46 +10,61 @@ open Microsoft.AspNetCore.Http
 open Microsoft.AspNetCore.TestHost
 open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Logging
 open Microsoft.Extensions.Hosting
 open Microsoft.Extensions.Options
-open FSharp.Control.Tasks
+open Microsoft.Extensions.Primitives
+open FSharp.Control.Tasks.V2.ContextInsensitive
+open Divergic.Logging.Xunit
 open Swensen.Unquote
 open Xunit
 
 // SUT
 open BitThicket.AspNetCore.Authentication
+open Xunit.Abstractions
 
-[<Fact>]
-[<Trait("Category", "Integration")>]
-let ``signature authenticated request to root``() = task {
-    let clientSecret = Guid.NewGuid().ToByteArray()
+type IntegrationTests(output:ITestOutputHelper) =
 
-    let builder = 
-        WebHostBuilder()
-            .ConfigureServices(
-                fun services -> 
-                    services.AddDistributedMemoryCache() |> ignore
-                    services.AddAuthentication("Signature")
-                        .AddScheme<SignatureAuthenticationOptions, SignatureAuthenticationHandler>("Signature", 
-                            fun (opts:SignatureAuthenticationOptions) -> 
-                                opts.Realm <- "Test"
-                                // opts.ClientSecretProvider <- { new IClientSecretProvider with
-                                //                                 member __.GetClientSecretAsync(_) = 
-                                //                                     Some clientSecret
-                                //                                     |> Task.FromResult }
-                                ())
-                    |> ignore)
-                        
-            .Configure(
-                fun app ->
-                    // app.UseAuthentication() |> ignore
-                    app.Run(fun context -> context.Response.WriteAsync("Hello World")) |> ignore
-                    app.UseAuthentication()
-                    |> ignore)
+    [<Fact>]
+    [<Trait("Category", "Integration")>]
+    member __.``signature required, but unauthenticated request to root``() = task {
+        let clientSecret = Guid.NewGuid().ToByteArray()
 
-    use server = new TestServer(builder)
+        let builder = 
+            WebHostBuilder()
+                .ConfigureServices(
+                    fun services -> 
+                        services
+                            .AddDistributedMemoryCache()
+                            .AddAuthentication("Signature")
+                            .AddScheme<SignatureAuthenticationOptions, SignatureAuthenticationHandler>("Signature", 
+                                fun (opts:SignatureAuthenticationOptions) -> 
+                                    opts.Realm <- "Test"
+                                    opts.ClientSecretProvider <- { new IClientSecretProvider with
+                                                                    member __.GetClientSecretAsync(_) = 
+                                                                        Some clientSecret
+                                                                        |> Task.FromResult }
+                                    ())
+                        |> ignore)
+                .ConfigureLogging(
+                    fun logging ->
+                        logging
+                            .AddFilter(fun _ -> true)
+                            .AddXunit(output)
+                        |> ignore)
+                .Configure(
+                    fun app ->
+                        app.UseAuthentication() |> ignore
+                        app.Run(fun context -> 
+                            task {
+                                if Seq.isEmpty context.User.Claims then
+                                    context.Response.StatusCode <- 401
+                                    context.Response.Headers.["WWW-Authenticate"] <- StringValues("Signature")
+                                else
+                                    do! context.Response.WriteAsync("Hello World")
+                            } :> Task))
+        use server = new TestServer(builder)
 
-    let! response = server.CreateClient().GetAsync("/")
-    printfn "testserver response: %A" response
-    test <@ response.StatusCode = HttpStatusCode.NotFound @>
-}
+        let! response = server.CreateClient().GetAsync("/")
+        test <@ response.StatusCode = HttpStatusCode.Unauthorized @>
+    }
