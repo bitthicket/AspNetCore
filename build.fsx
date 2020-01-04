7e630349155ecbb52791cdf2adac8a62b69d216b
Ben Collins <aggieben@gmail.com>
2020-01-04 11:32:21 -0600

    update pack target to derive version from tag

--- build.fsx
+++ build.fsx
@@ -1,16 +1,20 @@
 #r "paket:
+nuget Fake.Core.Target
 nuget Fake.DotNet.Cli
 nuget Fake.DotNet.Paket
 nuget Fake.IO.FileSystem
-nuget Fake.Core.Target //"
+nuget Fake.Tools.Git 
+//"
 #load ".fake/build.fsx/intellisense.fsx"
 
+open System.Text.RegularExpressions
 open Fake.Core
 open Fake.DotNet
 open Fake.IO
 open Fake.IO.FileSystemOperators
 open Fake.IO.Globbing.Operators
 open Fake.Core.TargetOperators
+open Fake.Tools.Git
 
 Target.initEnvironment ()
 
@@ -30,11 +34,17 @@ Target.create "Build" (fun _ ->
 )
 
 Target.create "Pack" (fun _ ->
+    //let tags = Information.getLastTag()
+    let tag = CommandHelper.runSimpleGitCommand __SOURCE_DIRECTORY__ "describe --contains"
+    if not(Regex.IsMatch(tag, @"^v[0-9]+\.[0-9]+\.[0-9]+(-[^^~]+)?$")) then 
+        failwithf "invalid versioning tag: %s" tag
+
     Paket.pack 
         (fun p ->
             { p with
                 ToolType = ToolType.CreateLocalTool()
                 BuildConfig = "Release"
+                Version = tag.Substring(1)
                 TemplateFile = "src/BitThicket.AspNetCore/paket.template" }))
 
 Target.create "Push" (fun _ ->
